40b7bc1bdced7291b93f27589aee744c43d56407
Orsolya Dee <dee.orsolya@gmail.com>
2009-08-09 17:06:35 +0200

    Noun corrections. Tests extended.

--- test/unit/mistake_test.rb
+++ test/unit/mistake_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class MistakeTest < ActiveSupport::TestCase
+  def test_belongs_to_user
+    assert_equals users(:pixiepal), mistakes(:one).user
+    assert_equals users(:globetrotter).to_set, [mistakes(:two).user, mistakes(:three).user].to_set
+  end
+
+  def test_belongs_to_word
+    assert_equals users(:pixiepal), mistakes(:one).user
+    assert_equals users(:globetrotter).to_set, [mistakes(:two).user, mistakes(:three).user].to_set
+  end
 
 end
