8c0c71e360b99fd157327b1ea2ccdce7db4df9f0
GT <norbert.tothgati@gmail.com>
2009-08-17 23:02:30 +0200

    Fix rails,ajax,charset problems. Removed temporary files.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,8 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :set_charset
   helper :all # include all helpers, all the time
-
+  
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '6c093cbbb264f6180fb57dbfc36c7bcb'
@@ -12,4 +13,8 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  def set_charset  
+    str_type = request.xhr? ? 'javascript' : 'html'  
+    headers['Content-Type'] = "text/#{str_type}; charset=ISO-8859-1"
+  end
 end
