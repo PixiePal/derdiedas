f38a7fdbd53b70fc512e8f027838065047fba89c
Orsolya Toth-Gati <pixiepal@PixiePal.localdomain>
2009-08-27 22:28:25 +0200

    buttons for wrong answers added

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,30 +4,37 @@ class QuizController < ApplicationController
   def index
     @main_word = get_new_word
     session[:is_retry] = false
+    session[:display_button_der] = true
+    session[:display_button_die] = true
+    session[:display_button_das] = true
   end
   
   def guess
-    #TODO possible obtimization? - work directly with german word and article 
+    #TODO possible optimization? - work directly with german word and article 
     # as parameters, thus avoiding going to the database for the word
     @main_word = Word.find_by_id params[:word_id]
     @missed_word_to_dispaly = nil
     @is_new_word = false
     
     if (@main_word.article == params[:article_guess])
-        if session[:is_retry]
-          @missed_word_to_dispaly = @main_word
-        end
+      # Word guesed -> switch to new word
+      if session[:is_retry]
+        @missed_word_to_dispaly = @main_word
+      end
         
-        flash[:notice_good] = @main_word.article + ' '+ @main_word.german
-        flash[:notice_bad] = nil
+      flash[:notice_good] = @main_word.article + ' '+ @main_word.german
+      flash[:notice_bad] = nil
         
-        @main_word = get_new_word
-        @is_new_word = true
-        session[:is_retry] = false
+      @main_word = get_new_word
+      @is_new_word = true
+      session[:is_retry] = false
+      session[:display_button_der] = true
+      session[:display_button_die] = true
+      session[:display_button_das] = true
     else
-        session[:is_retry] = true
-        flash[:notice_bad] = 'Try again!'
-        flash[:notice_good] = nil
+      # Wrong guess
+      session[:is_retry] = true
+      session["display_button_#{params[:article_guess]}".to_sym] = false
     end  
   end
   
