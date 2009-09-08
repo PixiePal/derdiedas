1dc714f02fe88927a0e31a4edae4f60e5606e083
PixiePal <orsolya.tothgati@gmail.com>
2009-09-08 22:30:48 +0200

    Quiz - Guess logic refactored, simplifyed.

--- test/functional/quiz_controller_test.rb
+++ test/functional/quiz_controller_test.rb
@@ -2,23 +2,14 @@ require 'test_helper'
 
 class QuizControllerTest < ActionController::TestCase
   
-  def setup
-    session[:button_state_der] = :button
-    session[:button_state_die] = :button
-    session[:button_state_das] = :button
-    session[:is_retry] = :false
-    session[:good_button_for] = nil
-  end
-  
   def test_display_fresh_quiz
     get :index
     assert_response :success
     assert_template :index
     assert_not_nil assigns(:main_word)
-    assert session[:is_retry] == false
-    assert session[:button_state_der] == :button
-    assert session[:button_state_die] == :button
-    assert session[:button_state_das] == :button
+    assert assigns(:wrong_button_for) == nil
+    assert assigns(:good_button_for) == nil
+    assert assigns(:solved_word) == nil
     
     assert_select '#main_word', :count => 1
     assert_select '#link_to_dict', :count => 1
@@ -42,62 +33,49 @@ class QuizControllerTest < ActionController::TestCase
     assert_response :success
     assert_template :guess
     
-    assert session[:button_state_der] == :button
-    assert session[:button_state_die] == :wrong
-    assert session[:button_state_das] == :button
-    assert session[:is_retry] == true
-    assert session[:good_button_for] == nil
+    assert assigns(:wrong_button_for) == 'die'
+    assert assigns(:good_button_for) == nil
+    assert assigns(:solved_word) == nil
+
     
     xml_http_request :get, :guess, :word_id => words(:apfel), :article_guess => 'das'
     assert_response :success
     assert_template :guess
     
-    assert session[:button_state_der] == :button
-    assert session[:button_state_die] == :wrong
-    assert session[:button_state_das] == :wrong
-    assert session[:is_retry] == true
-    assert session[:good_button_for] == nil
+    assert assigns(:wrong_button_for) == 'das'
+    assert assigns(:good_button_for) == nil
+    assert assigns(:solved_word) == nil
     
     #TODO: Selenium - test the actions resulted from the guess.rjs with Selenium
   end
   
   def test_good_guess_from_first_attempt
-    xml_http_request :get, :guess, :word_id => words(:apfel), :article_guess => 'der'
+    xml_http_request :get, :guess, :word_id => words(:apfel).id, :article_guess => 'der'
     assert_response :success
     assert_template :guess
     
-    assert session[:button_state_der] == :button
-    assert session[:button_state_die] == :button
-    assert session[:button_state_das] == :button
-    assert session[:is_retry] == false
-    assert session[:good_button_for] == 'der'
+    assert assigns(:good_button_for) == 'der'
+    assert assigns(:solved_word) == words(:apfel)
   end
   
   def test_good_guess_from_second_attempt
-    xml_http_request :get, :guess, :word_id => words(:apfel), :article_guess => 'die'
+    xml_http_request :get, :guess, :word_id => words(:ball).id, :article_guess => 'die'
     assert_response :success
     assert_template :guess
     
-    assert session[:button_state_der] == :button
-    assert session[:button_state_die] == :wrong
-    assert session[:button_state_das] == :button
-    assert session[:is_retry] == true
-    assert session[:good_button_for] == nil
+    assert assigns(:wrong_button_for) == 'die'
+    assert assigns(:good_button_for) == nil
+    assert assigns(:solved_word) == nil
     
-    xml_http_request :get, :guess, :word_id => words(:apfel), :article_guess => 'der'
+    xml_http_request :get, :guess, :word_id => words(:ball).id, :article_guess => 'der'
     assert_response :success
     assert_template :guess
 
     
-    assert session[:good_button_for] == 'der'
-  
-    # Test that preparations for the next word have been made
-    assert session[:button_state_der] == :button
-    assert session[:button_state_die] == :button
-    assert session[:button_state_das] == :button
-    assert session[:is_retry] == false
-    
+    assert assigns(:good_button_for) == 'der'
     assert assigns(@main_word) != nil
+    
+    assert assigns(:solved_word) == words(:ball)
   end
   
 end
